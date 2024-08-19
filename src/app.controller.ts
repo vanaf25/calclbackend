a385946b195e85ee721d144415bc622f049b9e28
vanaf25 <vanayfefilov777@gmail.com>
2024-08-19 18:27:52 +0200

    initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateCalculationDto } from './dto/calculation.dto';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
